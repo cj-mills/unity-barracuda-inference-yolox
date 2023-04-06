d8d51dea9305a5b46e683999505a15895fa33372
Christian J. Mills <9126128+cj-mills@users.noreply.github.com>
2023-04-06 16:59:33 -0700

    Create AddCustomDefineSymbol.cs

---
+++ Editor/AddCustomDefineSymbol.cs
@@ -0,0 +1,24 @@
+// using UnityEditor;
+// using UnityEngine;
+
+// namespace CJM.BarracudaInferenceToolkit.YOLOX
+// {
+//     public class DependencyDefineSymbolAdder
+//     {
+//         private const string CustomDefineSymbol = "CJM_BARRACUDA_INFERENCE_YOLOX";
+
+//         [InitializeOnLoadMethod]
+//         public static void AddCustomDefineSymbol()
+//         {
+//             var buildTargetGroup = EditorUserBuildSettings.selectedBuildTargetGroup;
+//             var defines = PlayerSettings.GetScriptingDefineSymbolsForGroup(buildTargetGroup);
+
+//             if (!defines.Contains(CustomDefineSymbol))
+//             {
+//                 defines += $";{CustomDefineSymbol}";
+//                 PlayerSettings.SetScriptingDefineSymbolsForGroup(buildTargetGroup, defines);
+//                 Debug.Log($"Added custom define symbol '{CustomDefineSymbol}' to the project.");
+//             }
+//         }
+//     }
+// }
