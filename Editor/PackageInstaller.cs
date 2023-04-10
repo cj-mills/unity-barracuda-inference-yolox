a7b50a0c478cfcec8465a8f34a7a6e12b68b2c92
Christian J. Mills <9126128+cj-mills@users.noreply.github.com>
2023-04-10 11:48:29 -0700

    Update PackageInstaller.cs

--- Editor/PackageInstaller.cs
+++ Editor/PackageInstaller.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-namespace CJM.BarracudaInferenceToolkit.YOLOX
+namespace CJM.BarracudaModelRunner.YOLOX
 {
     [System.Serializable]
     public class PackageData
