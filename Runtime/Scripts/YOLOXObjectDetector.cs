bd16d2c242f1c9c98966a46ce8353ebad612dec1
Christian J. Mills <9126128+cj-mills@users.noreply.github.com>
2023-04-10 11:44:14 -0700

    Update YOLOXObjectDetector.cs

--- Runtime/Scripts/YOLOXObjectDetector.cs
+++ Runtime/Scripts/YOLOXObjectDetector.cs
@@ -5,11 +5,11 @@ using System;
 using System.Collections.Generic;
 using Unity.Barracuda;
 
-#if CJM_BARRACUDA_INFERENCE_TOOLKIT && CJM_BBOX_2D_TOOLKIT && CJM_YOLOX_UTILS
+#if CJM_BARRACUDA_INFERENCE && CJM_BBOX_2D_TOOLKIT && CJM_YOLOX_UTILS
 using CJM.YOLOXUtils;
 using CJM.BBox2DToolkit;
 
-namespace CJM.BarracudaInferenceToolkit.YOLOX
+namespace CJM.BarracudaInference.YOLOX
 {
     /// <summary>
     /// YOLOXObjectDetector is a class that extends BarracudaModelRunner for object detection using the YOLOX model.
